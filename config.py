16bead70fc1929fb07b04922fa02452fc1ef3854
Dikiblood <dikiblood@bk.ru>
2021-02-19 15:18:05 +0300

    add config

---
+++ config.py
@@ -0,0 +1,9 @@
+USERNAME = ''
+PASSWORD = ''
+
+PROFILE = r''
+BATCH_SIZE = 50  # min = 1, max = 50
+
+NEW_COOKIE = False # need 1 time to get cookies
+STORIES_DOWNLOAD = True
+MEDIA_DOWNLOAD = True
