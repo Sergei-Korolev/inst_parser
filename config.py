a925f504101db060a9e721abd0665c1c569e3a69
Dikiblood <dikiblood@bk.ru>
2021-02-19 15:20:23 +0300

    del cached config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-USERNAME = ''
-PASSWORD = ''
-
-PROFILE = r''
-BATCH_SIZE = 50  # min = 1, max = 50
-
-NEW_COOKIE = False # need 1 time to get cookies
-STORIES_DOWNLOAD = True
-MEDIA_DOWNLOAD = True
